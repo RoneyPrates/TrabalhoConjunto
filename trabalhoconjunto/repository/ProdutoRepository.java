ab0008469f4e0d669b2d06cba2aa4abf59e072c3
Roney <107938728+RoneyPrates@users.noreply.github.com>
2024-06-03 22:07:59 -0300

    Delete trabalhoconjunto directory

--- trabalhoconjunto/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unipar.programacaoweb.trabalhoconjunto.repository;
-
-import br.unipar.programacaoweb.trabalhoconjunto.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
--- trabalhoconjunto/repository/VendaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.unipar.programacaoweb.trabalhoconjunto.repository;
-
-import br.unipar.programacaoweb.trabalhoconjunto.model.Venda;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VendaRepository extends JpaRepository<Venda, Integer> {
-}
