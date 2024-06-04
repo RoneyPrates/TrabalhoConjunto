b4eb824743a3b94ad34a8b40d5a4cb934b417fef
Roney <107938728+RoneyPrates@users.noreply.github.com>
2024-06-03 21:39:40 -0300

    Add files via upload

---
+++ Trabalho-Conjunto/src/main/java/br/unipar/programacaoweb/trabalhojulianepaulinho/model/Venda.java
@@ -0,0 +1,29 @@
+package br.unipar.programacaoweb.trabalhojulianepaulinho.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String observacoes;
+
+    private String data ;
+
+    private Double total;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id" )
+    private Cliente cliente;
+
+
+
+}
